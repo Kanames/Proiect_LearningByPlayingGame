6ee70cf4a0c297099d603f10708d16758e1521b9
Stefan <Stefan>
2018-03-11 21:02:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/stefan/proiect_learningbyplayinggame/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.stefan.proiect_learningbyplayinggame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
