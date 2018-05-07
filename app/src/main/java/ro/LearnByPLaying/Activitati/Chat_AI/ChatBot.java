b969ab2b543909c29345f4c1abf6fdfebb3b5872
Stefan <Stefan>
2018-05-07 21:04:23 +0300

    v0.1 ChatBOT

--- app/src/main/java/ro/LearnByPLaying/Activitati/Chat_AI/ChatBot.java
+++ app/src/main/java/ro/LearnByPLaying/Activitati/Chat_AI/ChatBot.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,28 +14,42 @@ import com.example.stefan.proiect_learningbyplayinggame.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class ChatBot extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ChatBot extends AppCompatActivity {
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems=new ArrayList<String>();
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+    private String HUMAN = "Human: ";
+    private String BOT = "Ben: ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_bot);
+        final ListView list = findViewById(R.id.list_of_messages);
 
-        FloatingActionButton fab =
-                (FloatingActionButton) findViewById(R.id.fab);
+        // Adapter: You need three parameters 'the context, id of the layout (it will be where the data is shown),
+        // and the array that contains the data
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, listItems);
 
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText input = (EditText) findViewById(R.id.input);
-
-                // Read the input field and push a new instance
-                // of ChatMessage to the Firebase database
-
-                // Clear the input
-                input.setText("");
+                EditText question = (EditText) findViewById(R.id.input);
+                adapter= new ArrayAdapter<String>(ChatBot.this, android.R.layout.simple_list_item_1, listItems);
+                adapter.add(HUMAN +question.getText().toString());
+                adapter.notifyDataSetChanged();
+                list.setAdapter(adapter);
+                question.setText("");
             }
         });
 
+
     }
+
 }
